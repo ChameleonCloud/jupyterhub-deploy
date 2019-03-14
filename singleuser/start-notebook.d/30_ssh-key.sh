a775dbd0125c4031e6b5d6e2f8df7437e515a4e3
Jason <jasonanderson@uchicago.edu>
2019-03-14 09:52:18 -0500

    Don't rewrite SSH key

--- singleuser/start-notebook.d/30_ssh-key.sh
+++ singleuser/start-notebook.d/30_ssh-key.sh
@@ -2,6 +2,8 @@
 # to serve as a deploy key, or uploaded as an OpenStack/AWS keypair.
 key_file="/home/jovyan/.ssh/id_rsa"
 
-mkdir -p "$(dirname "$key_file")"
-ssh-keygen -f "$key_file" -t rsa -b 4096 -C "$NB_USER@jupyterhub" -N ""
-chmod 400 "$key_file"
+if [[ ! -f "$key_file" ]]; then
+  mkdir -p "$(dirname "$key_file")"
+  ssh-keygen -f "$key_file" -t rsa -b 4096 -C "$NB_USER@jupyterhub" -N ""
+  chmod 400 "$key_file"
+fi
