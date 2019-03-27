4962e1b4ecf8196552b05bc81b48b6211b18ead0
Jason <jasonanderson@uchicago.edu>
2019-03-27 12:27:54 -0500

    Add checkout of community notebooks

--- singleuser/start-notebook.d/10_workdir.sh
+++ singleuser/start-notebook.d/10_workdir.sh
@@ -1,6 +1,9 @@
 # Copy examples and other "first launch" files over.
 rsync -aq /etc/jupyter/serverroot/ /work/
 
+# Check out community notebooks
+git clone https://github.com/chameleoncloud/notebooks.git /work/notebooks
+
 # Our volume mount is at the root directory, link it in to the user's
 # home directory for convenience.
 rm -rf /home/jovyan/work && ln -s /work /home/jovyan/work
