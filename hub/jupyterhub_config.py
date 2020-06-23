821b458f724cc766578d7029943efead5a58389f
Jason Anderson <jasonanderson@uchicago.edu>
2020-06-22 19:00:55 -0500

    Document all env vars

--- hub/jupyterhub_config.py
+++ hub/jupyterhub_config.py
@@ -1,31 +1,39 @@
 # Copyright (c) University of Chicago.
 # Distributed under the terms of the Modified BSD License.
-
+#
 # Configuration file for JupyterHub
 # The following environment variables affect the configuration at runtime:
 #
-# DEBUG:
-# JUPYTERHUB_BASE_URL:
-# DATA_VOLUME_CONTAINER:
-# DOCKER_VOLUME_DRIVER:
-# DOCKER_VOLUME_DRIVER_OPTS:
-# DOCKER_NOTEBOOK_IMAGE:
-# DOCKER_NETWORK_NAME:
-# KEYCLOAK_SERVER_URL:
-# KEYCLOAK_REALM_NAME:
-# KEYCLOAK_CLIENT_ID:
-# KEYCLOAK_CLIENT_SECRET:
-# OS_AUTH_URL:
-# OS_REGION_NAME:
-# OS_IDENTITY_PROVIDER:
-# OS_PROTOCOL:
-# MYSQL_HOST:
-# MYSQL_USER:
-# MYSQL_PASSWORD:
-# MYSQL_DATABASE:
-# CHAMELEON_SHARING_PORTAL_UPLOAD_URL:
-# CHAMELEON_SHARING_PORTAL_UPDATE_URL:
-#
+# DEBUG: (bool) whether to enable debug logging
+# JUPYTERHUB_BASE_URL: (str) the full (public) base URL of the JupyterHub server
+# DATA_VOLUME_CONTAINER: (str) the directory that JupyterHub will store its
+#                        runtime data in
+# DOCKER_VOLUME_DRIVER: (str) the name of the Docker volume driver to use when
+#                       creating user work directories
+# DOCKER_VOLUME_DRIVER_OPTS: (str) options, comma-separated "key=value" pairs,
+#                            passed to the volume create command
+# DOCKER_NOTEBOOK_IMAGE: (str) the name of the Docker image to spawn for users
+# DOCKER_NETWORK_NAME: (str) the Docker network name
+# KEYCLOAK_SERVER_URL: (str) the full base URL of the Keycloak server
+# KEYCLOAK_REALM_NAME: (str) the Keycloak realm name to authenticate against
+# KEYCLOAK_CLIENT_ID: (str) the Keycloak client ID
+# KEYCLOAK_CLIENT_SECRET: (str) the Keycloak client secret
+# OS_AUTH_URL: (str) the full base URL of the Keystone public endpoint
+# OS_REGION_NAME: (str) an optional default Keystone region; if not set, the
+#                 first detected region is used when looking up services
+# OS_IDENTITY_PROVIDER: (str) the Keystone identity provider to use when logging
+#                       in via federated authentication
+# OS_PROTOCOL: (str) the Keystone federation protocol to use (openid, saml)
+# MYSQL_HOST: (str) the JupyterHub MySQL hostname
+# MYSQL_USER: (str) the JupyterHub MySQL user (must have grants to database)
+# MYSQL_PASSWORD: (str) the JupyterHub MySQL user password
+# MYSQL_DATABASE: (str) the JupyterHub MySQL database used to store data
+# CHAMELEON_SHARING_PORTAL_UPLOAD_URL: (str) the full URL for the endpoint in
+#                                      the Chameleon sharing portal that starts
+#                                      the artifact creation flow.
+# CHAMELEON_SHARING_PORTAL_UPDATE_URL: (str) the full URL for the endpoint in
+#                                      the Chameleon sharing portal that starts
+#                                      the update flow.
 import os
 import sys
 
