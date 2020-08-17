752fba05f3ca0d0d5b4cb1d393138aaedbef19ca
Jason Anderson <jasonanderson@uchicago.edu>
2020-08-17 17:48:08 -0500

    Add wrapper function to refresh access tokens

---
+++ singleuser/bashrc.d/auth.sh
@@ -0,0 +1,16 @@
+# Install wrapper functions that perform lazy refresh of access tokens.
+# Access tokens have a relatively short TTL and can expire quickly.
+
+_with_token() {
+  access_token=$(curl -H"authorization: token $JUPYTERHUB_API_TOKEN" \
+    "$JUPYTERHUB_API_URL/tokens" \
+    | jq -r .access_token)
+  if [[ "$access_token" != "null" ]]; then
+    export OS_ACCESS_TOKEN="$access_token"
+  fi
+  command "$@"
+}
+export -f _with_token
+
+alias openstack='_with_token openstack'
+alias blazar='_with_token blazar'
