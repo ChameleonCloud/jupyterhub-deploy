70059bf5cc8a6729f1703f90ac07a8473a938991
Jason Anderson <jasonanderson@uchicago.edu>
2020-08-18 17:33:04 -0500

    Fix symlink failures on subsequent launches

--- singleuser/before-notebook.d/10_workdir.sh
+++ singleuser/before-notebook.d/10_workdir.sh
@@ -2,7 +2,7 @@
 # This helps `docker exec` continue to work (the container's launch dir
 # is /home/jovyan by default) and is really the only reason we do this.
 if [[ "$NB_USER" != jovyan ]]; then
-  ln -s "/home/$NB_USER" /home/jovyan
+  ln -sf "/home/$NB_USER" /home/jovyan
 fi
 
 # Fix permissions on entire home directory
