2580bbd6ef10b422aac6c6a6e1fcb05f9612320c
Jason Anderson <jasonanderson@uchicago.edu>
2021-04-02 11:07:09 -0500

    Disable chameleon theme

--- notebook/start-notebook.d/20_jupyterlab-settings.sh
+++ notebook/start-notebook.d/20_jupyterlab-settings.sh
@@ -3,9 +3,10 @@ user_settings_dir="/home/jovyan/.jupyter/lab/user-settings"
 
 theme_settings="$user_settings_dir/@jupyterlab/apputils-extension/themes.jupyterlab-settings"
 mkdir -p "$(dirname "$theme_settings")"
+# TODO: re-enable when Chameleon theme works / has some actual value.
 cat >"$theme_settings" <<EOF
 {
   // Override default theme
-  "theme": "Chameleon"
+  //"theme": "Chameleon"
 }
 EOF
