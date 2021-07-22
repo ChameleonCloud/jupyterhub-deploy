9017ef56a75dfd0e2a859adfa3ed278fb14cfa84
Jason Anderson <796413+diurnalist@users.noreply.github.com>
2021-07-22 15:56:41 -0500

    Automatically convert existing OpenSSH keys

--- notebook/start-notebook.d/30_ssh-key.sh
+++ notebook/start-notebook.d/30_ssh-key.sh
@@ -6,6 +6,13 @@ if [[ ! -f "$key_file" ]]; then
   mkdir -p "$(dirname "$key_file")"
   ssh-keygen -f "$key_file" -t rsa -m pem -b 4096 -C "$NB_USER@jupyterhub" -N ""
   chmod 400 "$key_file"
+elif grep -q 'BEGIN OPENSSH PRIVATE KEY' "$key_file"; then
+  # We used to generate key files with rfc4716, but older
+  # versions of Paramiko (<2.7) do not support it.
+  # https://github.com/paramiko/paramiko/issues/602
+  # This is a trick to "update" the key file by setting the
+  # passphrase to the same value, but it exports as PEM.
+  ssh-keygen -p -N "" -m pem -f "$key_file"
 fi
 
 # For backwards compatibility, also copy to /work.
