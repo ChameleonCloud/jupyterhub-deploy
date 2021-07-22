01ff683fe89983f828f7080cce93ac0d7b6dc63e
Adam Cooper <adamcooper@uchicago.edu>
2021-07-22 16:59:16 -0400

    Copy SSH public key to work dir (#5)
    The ssh-key hub setup script will now copy the public key to the working directory since it seems to be something that paramiko is expecting. It also ensures the key is in RSA format. This should fix the SSH issues some users were encountering in JupyterHub.
    
    Fixes ChameleonCloud/python-chi#13.
    
    Co-authored-by: Jason Anderson <796413+diurnalist@users.noreply.github.com>

--- notebook/start-notebook.d/30_ssh-key.sh
+++ notebook/start-notebook.d/30_ssh-key.sh
@@ -4,8 +4,16 @@ key_file="/data/.ssh/id_rsa"
 
 if [[ ! -f "$key_file" ]]; then
   mkdir -p "$(dirname "$key_file")"
-  ssh-keygen -f "$key_file" -t rsa -b 4096 -C "$NB_USER@jupyterhub" -N ""
+  ssh-keygen -f "$key_file" -t rsa -m pem -b 4096 -C "$NB_USER@jupyterhub" -N ""
   chmod 400 "$key_file"
+elif grep -q 'BEGIN OPENSSH PRIVATE KEY' "$key_file"; then
+  # We used to generate key files with rfc4716, but older
+  # versions of Paramiko (<2.7) do not support it.
+  # https://github.com/paramiko/paramiko/issues/602
+  # This is a trick to "update" the key file by setting the
+  # passphrase to the same value, but it exports as PEM.
+  cp -p "$key_file" "$key_file".bak
+  ssh-keygen -p -N "" -m pem -f "$key_file"
 fi
 
 # For backwards compatibility, also copy to /work.
@@ -13,4 +21,5 @@ work_key_file="/work/.ssh/id_rsa"
 if [[ ! -f "$work_key_file" ]]; then
   mkdir -p "$(dirname "$work_key_file")"
   cp "$key_file" "$work_key_file"
+  cp "${key_file}.pub" "${work_key_file}.pub"
 fi
