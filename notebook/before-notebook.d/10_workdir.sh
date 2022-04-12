0247612cf7c408bbfb4fb52b81a604f698c87bc6
Mark Powers <markpowers@uchicago.edu>
2022-04-12 11:21:04 -0500

    Add support for git import

--- notebook/before-notebook.d/10_workdir.sh
+++ notebook/before-notebook.d/10_workdir.sh
@@ -28,7 +28,15 @@ git_fetch() {
   local checkout="$2"
 
   if [[ ! -d "$checkout/.git" ]]; then
-    git clone "$remote" $checkout
+    # Splits the remote into remote@ref. Note we must always ensure to include
+    # an @ here, or the checkout will fail
+    remote_url=${remote%@*}
+    reference=${remote#*@}
+    git clone "$remote_url" $archivedir
+    pushd $archivedir
+    git archive --format=tar "$reference" | tar xf - -C "$checkout"
+    popd
+    rm -rf $archivedir
   else
     git_fetch_latest $checkout
   fi
@@ -41,12 +49,14 @@ setup_default_server() {
 }
 
 setup_experiment_server() {
-  if [[ "${ARTIFACT_DEPOSITION_REPO:-}" == "git" ]]; then
-    git_fetch "$ARTIFACT_CONTENTS_URL" $workdir
-  else
+  if [[ -n "${ARTIFACT_CONTENTS_URL:-}" ]]; then
+    echo "Downloading via wget"
     mkdir -p $archivedir
     wget -P $archivedir "$ARTIFACT_CONTENTS_URL"
     archivefile="$archivedir/$(find $archivedir -type f -exec basename {} \; | head -n1)"
+  elif [[ "${ARTIFACT_DEPOSITION_REPO:-}" == "git" ]]; then
+    echo "Fetching with git"
+    git_fetch "$ARTIFACT_DEPOSITION_URL" $workdir
   fi
   # Deposition URL may contain sensitive information (e.g. creds that are
   # valid for some TTL.)
@@ -72,7 +82,7 @@ setup_experiment_server() {
   # rm -rf /home/jovyan/exp && ln -s $expdir /home/jovyan/exp
 }
 
-if [[ -n "${ARTIFACT_CONTENTS_URL}" ]]; then
+if [[ -n "${ARTIFACT_DEPOSITION_REPO}" ]]; then
   setup_experiment_server
 else
   setup_default_server
