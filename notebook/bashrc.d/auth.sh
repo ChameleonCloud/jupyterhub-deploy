d71f1cc2ba636ae8c1d12e76031a3bf6cacd5a74
Adam Cooper <adamcooper@uchicago.edu>
2022-07-28 10:15:30 -0500

    Output user's project on shell start (#13)
    Some code has been added to the `bashrc` which will inform the user of which project they're using, or if they need to set one manually. This is complementary to ChameleonCloud/jupyterhub-chameleon#7

--- notebook/bashrc.d/auth.sh
+++ notebook/bashrc.d/auth.sh
@@ -16,3 +16,12 @@ alias openstack='_with_token openstack'
 alias blazar='_with_token blazar'
 # Users shouldn't really be using this, but they try sometimes.
 alias swift='_with_token swift'
+
+if [[ -n "${OS_PROJECT_NAME+set}" ]]; then
+  # Print project information in bold and blue so that it is noticeable
+  echo -e "\e[1mWorking on project \e[34;1m${OS_PROJECT_NAME}\e[;1m.\e[0m"
+  echo -e "\e[1mTo use a different project, set the \e[34;1mOS_PROJECT_NAME\e[;1m environment variable.\e[0m"
+else
+  echo -e "\e[31;1mThere is currently no project set! \e[0m"
+  echo -e "\e[1mPlease set the \e[34;1mOS_PROJECT_NAME\e[;1m environment variable.\e[0m"
+fi
