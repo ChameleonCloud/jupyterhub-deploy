84ea2cc27241f2d4938028ceb639a1f476c4cfc2
Adam Cooper <adamcooper@uchicago.edu>
2022-07-28 09:38:51 -0500

    Make project name message bold, as to be more noticeable

--- notebook/bashrc.d/auth.sh
+++ notebook/bashrc.d/auth.sh
@@ -19,8 +19,8 @@ alias swift='_with_token swift'
 
 if [[ -n "${OS_PROJECT_NAME+set}" ]]; then
   # Print project information in bold and blue so that it is noticeable
-  echo -e "Working on project \e[34;1m${OS_PROJECT_NAME}\e[0m."
-  echo -e "To use a different project, set the \e[34;1mOS_PROJECT_NAME\e[0m environment variable."
+  echo -e "\e[1mWorking on project \e[34;1m${OS_PROJECT_NAME}\e[;1m.\e[0m"
+  echo -e "\e[1mTo use a different project, set the \e[34;1mOS_PROJECT_NAME\e[;1m environment variable.\e[0m"
 else
   echo -e "\e[31;1mThere is currently no project set! \e[0m"
   echo -e "\e[1mPlease set the \e[34;1mOS_PROJECT_NAME\e[;1m environment variable.\e[0m"
